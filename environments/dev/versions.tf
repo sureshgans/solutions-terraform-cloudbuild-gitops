797662e526d6001915d78bf1efadcbcb17710f50
Sureshkumar Ganesan <suresh.gans@gmail.com>
2022-03-17 20:01:30 +0000

    Update project IDs and buckets

--- environments/dev/versions.tf
+++ environments/dev/versions.tf
@@ -14,5 +14,5 @@
 
 
 terraform {
-  required_version = "~> 1.1.7"
+  required_version = "~> 1.1.0"
 }
--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "PROJECT_ID-tfstate"
+    bucket = "gcp-devops2021-tfstate"
     prefix = "env/prod"
   }
 }
